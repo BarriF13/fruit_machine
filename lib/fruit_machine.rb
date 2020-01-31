b6c70b8a556ac969859cb97a9ccdba99e72be6b2
BarriF13 <info@bdesignstudio.co.uk>
2020-01-31 15:59:00 +0000

    Add credit and play

--- lib/fruit_machine.rb
+++ lib/fruit_machine.rb
@@ -1,8 +1,20 @@
 class FruitMachine
+  SINGLE_PLAY_FEE = 5
+
+  attr_reader :game_credits
+
   def initialize
     @slots = ['Black', 'White','Green','Yellow']
+    @game_credits = 0
+    @money = 0
   end
-  def display
+  def pull_lever
     @slots.sample(4).join(" | ")
+    @game_credits -=1
+  end
+
+  def insert(cash)
+    @money += cash
+    @game_credits += cash / SINGLE_PLAY_FEE
   end
 end
--- spec/fruit_machine_spec.rb
+++ spec/fruit_machine_spec.rb
@@ -1,14 +1,28 @@
 require 'fruit_machine'
 describe FruitMachine do
-  describe'#display' do
+  describe'#pull_lever' do
     it "should return slots " do
-      allow(subject).to receive(:display).and_return("Black | White | Green | Yellow")
-    expect(subject.display).to eq ('Black | White | Green | Yellow')
+      subject.insert(5)
+      allow(subject).to receive(:pull_lever).and_return("Black | White | Green | Yellow")
+    expect(subject.pull_lever).to eq ('Black | White | Green | Yellow')
     end
 
     it " randomly displays slots of colors" do
-      allow(subject).to receive(:display).and_return("Black | White | Green | Yellow")
-      expect(subject.display).to eq ('Black | White | Green | Yellow')
+      subject.insert(5)
+      allow(subject).to receive(:pull_lever).and_return("Black | White | Green | Yellow")
+      expect(subject.pull_lever).to eq ('Black | White | Green | Yellow')
+    end
+    it "should deduce a credit when pulling the lever" do
+      subject.insert(5)
+      expect(subject.game_credits).to eq(1)
+      subject.pull_lever
+      expect(subject.game_credits).to eq(0)
+    end
+  end
+  describe '#insert'do
+    it "gives game credit for every £5 inserted" do
+      subject.insert(5)
+      expect(subject.game_credits).to eq 1
     end
   end
 end
